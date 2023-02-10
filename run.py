b247c994f1f21c1878860699b92e4db39794d5c7
yemoalakija <adeyemo.alakija@gmail.com>
2023-02-10 14:57:31 +0000

    creates a 2-dimentional list that represent the game board or field

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from termcolor import colored
+
+player = []
+opponent = []
+player_guesses = []
+
+
+# This creates a 2-dimentional list that represent the game board or field as
+# I call it. The loop runs 5 times and appends a row of 5 "X" to the "field"
+# list, creating a 5x5 game field.
+# filled with "X"s
+def battle_ground():
+    field = []
+    for i in range(5):
+        row = [" X "] * 5
+        field.append(row)
+    return field
